97f9b749742c0079a44020dd8e2c6d3aa5a05b2e
Shayne <shayne@nomad.ca>
2012-01-22 16:36:00 +0000

    added a field only render

--- uni_form/templatetags/manual_form_field.py
+++ uni_form/templatetags/manual_form_field.py
@@ -7,3 +7,10 @@ def manual_form_field(field):
     return {
             'field': field
             }
+
+@register.inclusion_tag('uni_form/field.html')
+def manual_form_field_no_label(field):
+    return {
+            'field': field,
+            'nolabel': True
+            }
